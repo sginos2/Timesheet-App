480a50c2d4d6c408e83a347fbc8901b9d49d4669
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-07-22 07:31:10 -0600

    Completed Unit 11

--- Desktop/Timesheet-App/hr-timesheet/src/app/app.module.ts
+++ Desktop/Timesheet-App/hr-timesheet/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { MaterialModule } from './modules/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Desktop/Timesheet-App/hr-timesheet/src/app/components/departments/departments.component.ts
+++ Desktop/Timesheet-App/hr-timesheet/src/app/components/departments/departments.component.ts
@@ -18,7 +18,9 @@ export class DepartmentsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.departments = this.departmentsService.departments;
+    this.departmentsService.getDeparments().subscribe(departments => {
+      this.departments = departments;
+    });
   }
 
   goToDepartment(departmentId: string): void {
